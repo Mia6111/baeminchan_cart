ea350afa2a0cec51c9005402db73277afd8e9c82
Mi Hyon <mhyun1116@gmail.com>
2018-08-13 20:43:39 +0900

    compile error

--- src/main/java/codesquad/domain/ProductRepository.java
+++ src/main/java/codesquad/domain/ProductRepository.java
@@ -2,7 +2,6 @@ package codesquad.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import javax.jnlp.PersistenceService;
 import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
